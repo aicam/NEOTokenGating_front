28de4edf78d6a804861006b78e1cbd241638e1fd
ali <a_icam@yahoo.com>
2023-06-01 01:35:55 -0700

    todos

--- src/pages/buy_ticket.js
+++ src/pages/buy_ticket.js
@@ -13,28 +13,50 @@ const AFFILIATE_MARKETING_CONTRACT_ADDRESS = AffiliateMarketing.address;
 const affiliateContractABI = AffiliateMarketing.abi;
 
 export default function BuyTicket() {
-    const {address, connector, isConnected} = useAccount()
-    const {data, isLoading, isSuccess, write} = useContractWrite({
+    const {isConnected} = useAccount()
+    const {write} = useContractWrite({
         address: AFFILIATE_MARKETING_CONTRACT_ADDRESS,
         abi: affiliateContractABI,
         functionName: 'buyTicket',
     })
+
+    // TODO: call function to check if already bought NFT
+    const {checkNFT} = useContractWrite({
+        address: AFFILIATE_MARKETING_CONTRACT_ADDRESS,
+        abi: affiliateContractABI,
+        functionName: 'buyTicket',
+    })
+
+    // TODO: call function to get reamining tickets
+    const {getNumRemainingTickets} = useContractWrite({
+        address: AFFILIATE_MARKETING_CONTRACT_ADDRESS,
+        abi: affiliateContractABI,
+        functionName: 'buyTicket',
+    })
+
+    const buy = () => {
+        // TODO: check balance, chain Id and handle errors
+        write({
+            value: parseEther('0.001'),
+        })
+    }
+
     const router = useRouter();
     const { from } = router.query;
 
     useEffect(() => {
+        // TODO: Affiliate Marketing Address
         console.log('keywords here ', from)
     }, [from])
 
     return (
         <>
+            {/* TODO: check if NFT is already in wallet */}
             <p className="colorful-text" style={{fontSize: '35px', color: 'white', fontFamily: 'fantasy'}}>June 20 2023</p>
             <p style={{fontSize: '28px', marginTop: '-20px', color: 'white', textAlign: 'center'}}>3-5 PM</p>
             <Image src={require('../assets/skateboard.gif')} height={300} style={{margin: '10px'}}/>
             <p style={{fontSize: '28px', color: 'white', textAlign: 'center'}}><Image src={require('../assets/polygon-matic-logo.png')} width={30} />40</p>
-            {isConnected ? <Button as="a" href="#" disabled={!isConnected} onClick={() => write({
-                value: parseEther('0.001'),
-            })} filled>Buy Ticket</Button> : null}
+            {isConnected ? <Button as="a" href="#" disabled={!isConnected} onClick={buy} filled>Buy Ticket</Button> : null}
             <Web3Button icon="show" label="Connect Wallet" balance="show"/>
             <br/>
 
